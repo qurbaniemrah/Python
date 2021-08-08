a728fdb1c075f4e379288eb7021471f086553aad
qurbaniemrah <qurbaniemrah@outlook.com>
2021-08-08 13:33:54 +0400

    python practice edildi

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 @app.route("/")
-def hello_world():
+def index():
 
  return "<p>Hello</p>"
+
+@app.route("/about")
+def about():
+    return "<p>Haqqimda</p>"
 
 if __name__ == "__main__":
     app.run(debug = True)
