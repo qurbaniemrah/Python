8a55367a14a7fa275ba897eb4ddda2cfb55ba83a
qurbaniemrah <qurbaniemrah@outlook.com>
2021-08-08 14:05:10 +0400

    flaskla html connect olundu

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 @app.route("/")
 def index():
+    article = dict()
+    article["title"] = "Deneme"
+    article["body"] = "Deneme 123"
+    article["author"] = "Qurbani Emrahov"
 
- return "<p>Hello</p>"
+    return render_template("index.html",article = article)
 
-@app.route("/about")
-def about():
-    return "<p>Haqqimda</p>"
+
 
 if __name__ == "__main__":
     app.run(debug = True)
