cb919bc37ed084585bc935ed1932c802c98ffd01
qurbaniemrah <qurbaniemrah@outlook.com>
2021-08-09 21:13:30 +0400

    bootstrap elave edildi

--- app.py
+++ app.py
@@ -3,14 +3,10 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    article = dict()
-    article["title"] = "Deneme"
-    article["body"] = "Deneme 123"
-    article["author"] = "Qurbani Emrahov"
-
-    return render_template("index.html",article = article)
-
-
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
+
+
+    <p>nbjnjjbj</p>
