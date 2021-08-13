31d8c78fc9eaf0a72cf916cc99d1a6b8887cb814
qurbaniemrah <qurbaniemrah@outlook.com>
2021-08-13 20:34:18 +0400

    flask practice olundu

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    numbers = [1, 2, 3, 4, 5]
+    return render_template("index.html",answer = "evet",)
+@app.route("/about")
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-    <p>nbjnjjbj</p>
