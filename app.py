6715f32e9a7e58597c3da2887aa9aeed5ab168f6
qurbaniemrah <qurbaniemrah@outlook.com>
2021-08-15 09:23:18 +0400

    project ucun lazimli modullar yuklendi

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
+
+
+
+
+
 
 app = Flask(__name__)
 @app.route("/")
@@ -8,6 +16,9 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+@app.route("/article/<string:id>")
+def detail(id):
+    return "Article Id:" +id     
 if __name__ == "__main__":
     app.run(debug=True)
 
